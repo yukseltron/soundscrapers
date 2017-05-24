b01934b08e8fe284d03715db6c1476819a4840b6
Hamid <h.yuksel04@gmail.com>
2017-05-24 15:02:05 -0400

    now sorts by date, followed by price then alphabetical order

--- main.py
+++ main.py
@@ -4,36 +4,53 @@ import operator
 
 page = requests.get('http://www.soundscapesmusic.com/tickets/')
 tree = html.fromstring(page.content)
-
 gigs = tree.xpath('//tr[position()>1]//td')
+calendar = {'Jan': 1,'Feb': 2,'Mar': 3,'Apr': 4,
+            'May': 5,'Jun': 6,'June': 6,'Jul': 7,'Aug': 9,
+            'Sep': 9,'Oct': 10,'Nov': 11,'Dec': 12}
 data = []
 artists = []
 locations = []
 dates = []
 prices = []
 
-for i in range(0,len(gigs),4):
-    artist = gigs[i].text_content()
-    location = gigs[i+1].text_content()
-    date = gigs[i+2].text_content()
-    price = gigs[i+3].text_content()
+def getData():
+    for i in range(0,len(gigs),4):
+        artist = gigs[i].text_content()
+        location = gigs[i+1].text_content()
+        d = gigs[i+2].text_content()
+        date = d.split(" ")
+        month = getMonth(date)
+        day = getDay(date)
+        price = gigs[i+3].text_content()
+        deets = [artist, location, date, price,month,int(day)]
+        data.append(deets)
 
-    deets = [artist, location, date, price]
-    data.append(deets)
+def getMonth(date):
+    if len(date) > 2:
+        if date[1] in calendar:
+            return calendar[date[1]]
+    else:
+        return calendar[date[0]]
+
+def getDay(date):
+    if len(date) > 2:
+        day = date[2].split("-")
+        return day[0]
+    else:
+        day = date[1].split("-")
+        return day[0]
 
 def sortByArtists(data):
     data.sort(key=operator.itemgetter(0))
-    for i in data:
-        print(i)
+    return data
 
 def sortByDate(data):
-    data.sort(key=operator.itemgetter(2))
-    for i in data:
-        print(i)
+    sortedDataTemp = sorted(data, key=operator.itemgetter(4,5,3,0))
+    return sortedDataTemp:
 
 def sortByPrice(data):
     data.sort(key=operator.itemgetter(3))
-    for i in data:
-        print(i)
+    return data
 
-sortByDate(data)
+getData()

b70b499764dc1b5b21514f86f2f522fb28dacf75
Hamid <h.yuksel04@gmail.com>
2017-05-24 12:04:20 -0400

    fetches all data and ornganized into list

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import requests
 page = requests.get('http://www.soundscapesmusic.com/tickets/')
 tree = html.fromstring(page.content)
 
-gigs = tree.xpath('//td/text()')
+gigs = tree.xpath('//tr[position()>1]//td')
+data = []
+artists = []
+locations = []
+dates = []
+prices = []
 
-print('gigs',gigs)
+for i in range(0,len(gigs),4):
+    artist = gigs[i].text_content()
+    location = gigs[i+1].text_content()
+    date = gigs[i+2].text_content()
+    price = gigs[i+3].text_content()
+
+    deets = [artist, location, date, price]
+    data.append(deets)
